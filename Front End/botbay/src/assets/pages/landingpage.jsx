2d79d1a13946858037fce81a05f5ffb5a9abeb53
abcde456 <yellieth2575@hwdsb.on.ca>
2026-01-14 21:57:49 -0500

    Added full mobile support along with tablet support
    Next tasks:
    - Make sign up page
    - Connect to backend
    - Begin dashboard for inventory management

--- Front End/botbay/src/assets/pages/landingpage.jsx
+++ Front End/botbay/src/assets/pages/landingpage.jsx
@@ -9,19 +9,71 @@ import bookIcon from '../images/bookicon.png';
 import navlogo from '../images/BlankLogo.png';
 
 function LandingPage() {
-  const isDesktop = useMediaQuery({ query: '(min-width: 1400px)' });
+  const isDesktop = useMediaQuery({ query: '(min-width: 1100px)' });
+
   return (
     <div class="screencontainer">
       {isDesktop ? (
         <>
           <div class="navbar">
-          <div class="halfcontainer">
-            <div class="leftcontainer">
-              <img class="navlogo" src={navlogo}></img>
+            <div class="halfcontainer">
+              <div class="leftcontainer">
+                <img class="navlogo" src={navlogo}></img>
+              </div>
+            </div>
+            <div class="halfcontainer">
+              <div class="rightcontainer">
+                <div class="navtexthelper">
+                  <p class="navunderlinebutton">Home</p>
+                  <p class="navunderlinebutton">About</p>
+                  <button class="navbutton">Sign In</button>
+                </div>
+              </div>
             </div>
           </div>
-          <div class="halfcontainer">
-            <div class="rightcontainer">
+          <div class="herocontainer">
+            <div class="halfcontainer">
+              <div class="vertical33container">
+                <p class="herotitle">Keep Track of Your Team’s Parts With Ease</p>
+              </div>
+              <div class="vertical34container">
+                <div class="halfcontainer">
+                  <div class="leftcontainer">
+                    <ul>
+                      <li>Multi-device syncing</li>
+                      <li>Thorough interface</li>
+                      <li>Keeps you organized</li>
+                      <li>Useful utilities for all teams</li>
+                    </ul>
+                  </div>      
+                </div>
+                <div class="halfcontainer">
+                  <div class="rowcontainer">
+                    <img class="heroicon" src={multiDeviceIcon}></img>
+                    <img class="heroicon" src={listIcon}></img>
+                    <img class="heroicon" src={bookIcon}></img>
+                  </div>
+                </div>
+              </div>
+              <div class="vertical33container">
+                <div class="evenspreadflex">
+                  <button class="herobutton">Sign Up</button>
+                  <button class="herobutton">Try it Out</button>
+                </div>
+              </div>
+            </div>
+            <div class="halfcontainer">
+
+            </div>
+          </div>
+        </>
+      ) : (
+        <>
+          <div class="navbar">
+            <div class="halfcontainer">
+              <img class="navlogo" src={navlogo}></img>
+            </div>
+            <div class="halfcontainer">
               <div class="navtexthelper">
                 <p class="navunderlinebutton">Home</p>
                 <p class="navunderlinebutton">About</p>
@@ -29,45 +81,27 @@ function LandingPage() {
               </div>
             </div>
           </div>
-        </div>
-        <div class="herocontainer">
-          <div class="halfcontainer">
-            <div class="vertical33container">
-              <p class="herotitle">Keep Track of Your Team’s Parts With Ease</p>
-            </div>
-            <div class="vertical34container">
-              <div class="halfcontainer">
-                <div class="leftcontainer">
-                  <ul>
+          <div class="herocontainer">
+                <center>
+                  <p class="herotitle">Keep Track of Your Team’s Parts With Ease</p>
+                  <ul class="herolist">
                     <li>Multi-device syncing</li>
                     <li>Thorough interface</li>
                     <li>Keeps you organized</li>
                     <li>Useful utilities for all teams</li>
                   </ul>
-                </div>      
-              </div>
-              <div class="halfcontainer">
+                </center>
                 <div class="rowcontainer">
                   <img class="heroicon" src={multiDeviceIcon}></img>
                   <img class="heroicon" src={listIcon}></img>
                   <img class="heroicon" src={bookIcon}></img>
                 </div>
-              </div>
-            </div>
-            <div class="vertical33container">
-              <div class="evenspreadflex">
-                <button class="herobutton">Sign Up</button>
-                <button class="herobutton">Try it Out</button>
-              </div>
-            </div>
+                <div class="evenspreadflex">
+                  <button class="herobutton">Sign Up</button>
+                  <button class="herobutton">Try it Out</button>
+                </div>
           </div>
-          <div class="halfcontainer">
-
-          </div>
-        </div>
-      </>
-      ) : (
-        <p>this is a phone</p>
+        </>
       )}
     </div>
   );
