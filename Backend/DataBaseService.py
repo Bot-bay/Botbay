c0b762226e865f047a74c53bfa66515cb92eb376
Romon Home <romonm2008@gmail.com>
2026-01-16 09:56:08 -0500

    Implement product submission feature with authentication in AddProduct route and create corresponding HTML form. (NOTE NOT FINISHED, JUST NEED TO WORK ON IT SOMEWHERE ELSE)

--- Backend/DataBaseService.py
+++ Backend/DataBaseService.py
@@ -2,20 +2,29 @@ from dotenv import load_dotenv
 import supabase
 import os
 
-SERVER = False
 
 
 
-
-if SERVER == False:
+def InitializeEnviron():
     load_dotenv("Backend\.env") # So it can work on Virtual Environment
 
 
+def InitalizeDB():
+    URL : str = os.environ.get("DBURL")
+    KEY : str =  os.environ.get("DBKEY")
+    
+    DbClient = supabase.create_client(URL,KEY)
 
 
-URL : str = os.environ.get("DBURL")
-KEY : str =  os.environ.get("DBKEY")
+#Classes meant for organization
+
+class FrontEndRequests:
+    pass
 
 
+#For behind the scenes stuff
+class InternalRequests:
+    def AddProductToDB():
+        pass
 
-DbClient = supabase.create_client(URL,KEY)
+
--- Backend/app.py
+++ Backend/app.py
@@ -6,13 +6,18 @@
 import flask
 from flask_socketio import SocketIO, emit
 import os
+import base64
+import DataBaseService
 
+
+DataBaseService.InitializeEnviron() 
 
 
 App = flask.Flask(__name__)
 Websocket = SocketIO(App)
 
-
+if os.path.exists('templates'):
+    print(f"Files in templates: {os.listdir('templates')}")
 
 def ServerMessage(Status : str):
     emit("ServerMessage",{"Status":Status})
@@ -30,10 +35,37 @@ def Debug():
     return "Server is up!"
 
 
+@App.route("/AddProduct", methods=["GET", "POST"])
+def AddProduct():
+    Request = flask.request
+
+    if Request.method == "POST":
+        # Accessing JSON data sent via fetch
+        Data = Request.get_json()
+        
+        Auth = Data.get("Auth")
+        Name = Data.get("Name")
+        Images = Data.get("Urls")
+
+        if base64.b64decode(Auth) == os.environ["AuthCode"]:
+            return flask.jsonify({"Status": "Success", "Message": f"Product {Name} added"}), 200
+        else:
+            return flask.jsonify({"Status": "Error", "Message": f"Incorrect Authentication"}), 460
 
 
 
-Websocket.run(App)
+        
+
+    return flask.render_template("AddProduct.htm")
+
+
+        
+    
+
+
+
+
+Websocket.run(App,debug=True)
 
 
 
