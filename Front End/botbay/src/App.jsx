5325f5d74f0953cb4fddab44b8de1bae63d86cfb
abcde456 <yellieth2575@hwdsb.on.ca>
2026-01-15 19:00:50 -0500

    Cleaned up react + vite default icons, now use our icon for favicon

--- Front End/botbay/src/App.jsx
+++ Front End/botbay/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './assets/pages/landingpage.jsx';
 import SignUpPage from './assets/pages/signuppage.jsx'
