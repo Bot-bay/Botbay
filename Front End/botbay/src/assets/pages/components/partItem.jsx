1a2640eac8a079644abf43161dd03f31beac204d
abcde456 <yellieth2575@hwdsb.on.ca>
2026-01-27 18:43:31 -0500

    Added some more work on dropdown menu for tag selection

--- Front End/botbay/src/assets/pages/components/partItem.jsx
+++ Front End/botbay/src/assets/pages/components/partItem.jsx
@@ -1,13 +1,16 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 import { IoTrashSharp } from "react-icons/io5";
 import { FaPencilAlt } from "react-icons/fa";
 
 function PartItem({ part }) {
+    const [imgLoaded, setImgLoaded] = useState(true);
+
     return (
         <div className="d-partitem">
             <div style={{ width: '15%', display:'flex', justifyContent:'center', alignItems:'center', textAlign:'center' }}>
-                {part.icon && <img src={part.icon} style={{ width: '40px', height: '40px', marginRight:'15px' }} alt={`${part.name} icon`} />}
+                {part.icon && imgLoaded && 
+                    <img src={part.icon} style={{ width: '40px', height: '40px', marginRight:'15px' }} alt={`${part.name} icon`} onError={() => setImgLoaded(false)}/>
+                }
                 {part.manufacturerId}
             </div>
             <div style={{ width: '50%' }}>{part.name}</div>
