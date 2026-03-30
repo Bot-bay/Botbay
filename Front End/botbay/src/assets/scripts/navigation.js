c311aeb785db62c23356fe4fd12f9a1f478ac9d9
abcde456 <yellieth2575@hwdsb.on.ca>
2026-03-30 19:35:02 -0400

    Added proper wiki link

--- Front End/botbay/src/assets/scripts/navigation.js
+++ Front End/botbay/src/assets/scripts/navigation.js
@@ -1,6 +1,6 @@
 import { useNavigate } from "react-router-dom";
 
-export const WikiUrl = "https://github.com/UltraFish19/Bot-Bay";
+export const WikiUrl = "https://github.com/Bot-bay/Botbay/wiki";
 
 export function switchToPage(path) {
     const navigate = useNavigate();
